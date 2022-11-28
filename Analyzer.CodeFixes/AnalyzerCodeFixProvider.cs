0c930eb93cb62c902c69ab6f79b6737b13f4c3ea
Jesse Dubbink <s1153776@student.windesheim.nl>
2022-11-28 20:51:46 +0100

    Initial Commit

---
+++ Analyzer.CodeFixes/AnalyzerCodeFixProvider.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Simplification;
+using Analyzer.Analyzers;
+
+namespace Analyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AnalyzerCodeFixProvider)), Shared]
+    public class AnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(MethodAnalyzer.DiagnosticId, SingletonAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            if (diagnostic.Id == MethodAnalyzer.DiagnosticId)
+            {
+                // Find the type declaration identified by the diagnostic.
+                var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<CompilationUnitSyntax>().First();
+
+                // Register a code action that will invoke the fix.
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title: CodeFixResources.MethodFixTitle,
+                        createChangedDocument: c => MakePublicAsync(context.Document, declaration, c),
+                        equivalenceKey: nameof(CodeFixResources.MethodFixTitle)),
+                    diagnostic);
+            }
+        }
+
+        private static async Task<Document> MakePublicAsync(Document document, CompilationUnitSyntax classTree, CancellationToken cancellationToken)
+        {
+            //// Remove the leading trivia from the local declaration.
+            //MethodDeclarationSyntax methodDeclaration = classTree.DescendantNodes().OfType<MethodDeclarationSyntax>().FirstOrDefault();
+
+            //SyntaxTriviaList leadingTrivia = firstToken.LeadingTrivia;
+            //LocalDeclarationStatementSyntax trimmedLocal = methodDeclaration.Identifier();
+
+            //// Create a const token with the leading trivia.
+            //SyntaxToken constToken = SyntaxFactory.Token(leadingTrivia, SyntaxKind.ConstKeyword, SyntaxFactory.TriviaList(SyntaxFactory.ElasticMarker));
+
+            //// Insert the const token into the modifiers list, creating a new modifiers list.
+            //SyntaxTokenList newModifiers = trimmedLocal.Modifiers.Insert(0, constToken);
+
+            //// Add an annotation to format the new local declaration.
+            //LocalDeclarationStatementSyntax formattedLocal = newLocal.WithAdditionalAnnotations(Formatter.Annotation);
+
+
+
+            // Replace the old local declaration with the new local declaration.
+            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            //SyntaxNode newRoot = oldRoot.ReplaceNode(classTree, formattedLocal);
+
+            // Return document with transformed tree.
+            return document.WithSyntaxRoot(oldRoot);
+        }
+    }
+}
---
+++ Analyzer/Analyzers/SingletonAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Linq;
+
+namespace Analyzer.Analyzers
+{
+    public class SingletonAnalyzer
+    {
+        public const string DiagnosticId = "Singleton";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.SingletonTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.SingletonMessage), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.SingletonDescription), Resources.ResourceManager, typeof(Resources));
+
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerAnalyzer.Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public SingletonAnalyzer(SyntaxNodeAnalysisContext context)
+        {
+            var classTree = context.Node.SyntaxTree.GetRoot() as CompilationUnitSyntax;
+            var constructorDeclaration = classTree.DescendantNodes().OfType<ConstructorDeclarationSyntax>().FirstOrDefault();
+            var variableDeclaration = classTree.DescendantNodes().OfType<FieldDeclarationSyntax>().FirstOrDefault();
+
+            if (constructorDeclaration == null || constructorDeclaration == null)
+            {
+                return;
+            }
+
+            if (!constructorDeclaration.Modifiers.Any(SyntaxKind.PrivateKeyword))
+            {
+                return;
+            }
+
+            if (variableDeclaration != null && !variableDeclaration.Modifiers.Any(SyntaxKind.PrivateKeyword))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, constructorDeclaration.GetLocation()));
+        }
+    }
+}
