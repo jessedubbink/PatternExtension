8effd232d27298539cde75814848ddd5cbc09ac5
Michael <43703002+michaelkeizers@users.noreply.github.com>
2022-12-10 21:01:31 +0100

    added coreanalyzer.cs

---
+++ InspectorPatterns/InspectorPatterns.Core/CoreAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InspectorPatterns.Core
+{
+    public class CoreAnalyzer
+    {
+        public CoreAnalyzer(SyntaxNodeAnalysisContext context)
+        {
+
+        }
+    }
+}
--- InspectorPatterns/InspectorPatterns/InspectorPatternsAnalyzer.cs
+++ InspectorPatterns/InspectorPatterns/InspectorPatternsAnalyzer.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using InspectorPatterns.Core;
+using InspectorPatterns.Core.DesignPatterns.Analyzers;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -33,22 +35,12 @@ namespace InspectorPatterns
 
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
+            var analyzer = new CoreAnalyzer(context);
         }
     }
 }
