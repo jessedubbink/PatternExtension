4cbb62082953298975eba2aed991a1ad95eb3d12
Mohamed Kaddouri <S1172394@student.windesheim.nl>
2022-12-29 21:49:43 +0100

    started to add Decorator pattern with Justin and Michael

---
+++ InspectorPatterns/InspectorPatterns.Core/Analyzers/DecoratorAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿using InspectorPatterns.Core.Interfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Linq;
+
+namespace InspectorPatterns.Core.Analyzers
+{
+    public class DecoratorAnalyzer
+    {
+
+        public class HasSomeInterface : IAnalyzer
+        {
+            private readonly SyntaxNode _classTree;
+            private readonly SyntaxNodeAnalysisContext _context;
+            private string _identifierValue;
+
+
+            public HasSomeInterface(SyntaxNodeAnalysisContext context)
+            {
+                _context = context;
+            }
+
+            public bool Analyze()
+            {
+                /**
+                 * Heeft de class een interface.
+                 * Is de constructor public en komt die overeen met de interface.
+                 * Is er een public propperty met de zelfde interface
+                 * heeft een constructor de zelfde interface als argument 
+                 */
+                ClassDeclarationSyntax classSyntax = (ClassDeclarationSyntax)_context.Node;
+                if (null == classSyntax.BaseList)
+                {
+                    return false;
+                }
+                // lists interfaces that current class have implemented
+                var implementedInterfaces = classSyntax.BaseList.Types;
+                foreach (var implementedInterface in implementedInterfaces)
+                {
+                    // gets all classes in a project
+                    var allClasses = _context.Compilation.SyntaxTrees;
+                    foreach (var klass in allClasses)
+                    {
+                        // gets declared classes
+                        var classDeclarations = klass.GetRoot().DescendantNodesAndSelf(n => n is CompilationUnitSyntax || n is MemberDeclarationSyntax).OfType<ClassDeclarationSyntax>();
+                        var classDeclarationBaseList = classDeclarations.First().BaseList;
+                        if (null != classDeclarationBaseList)
+                        {
+                            var classListDeclarationTypes = classDeclarationBaseList.Types;
+                            foreach (var classDeclarationType in classListDeclarationTypes)
+                            {
+                                var type = (IdentifierNameSyntax)classDeclarationType.Type;
+                                // checks if current interface is same as implemented interface of declared class in project
+                                if ((IdentifierNameSyntax)implementedInterface.Type != type)
+                                {
+                                    continue;
+                                }
+
+                            }
+                        }
+                        //klass.GetRoot().
+                    }
+                }
+              
+         /*       _identifierValue = (string)classSyntax.Identifier.Value;
+                var fieldDeclarations = _classTree.DescendantNodes().OfType<interfaceSyntax.Identifier.Value>();
+         */
+                return true;
+            }
+
+            public string GetInterfaceName()
+            {
+                return _identifierValue;
+            }
+        }
+    }
+}
---
+++ InspectorPatterns/InspectorPatterns/DecoratorPatternAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using InspectorPatterns.Core;
+using InspectorPatterns.Core.Analyzers;
+using InspectorPatterns.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace InspectorPatterns
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DecoratorPatternAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SingletonPatternAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.SingletonTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.SingletonMessage), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.SingletonDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Singleton";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        private DesignPatternAnalyzer analyzer;
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            if (analyzer == null)
+            {
+                analyzer = new DesignPatternAnalyzer();
+            }
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSyntaxNodeAction(AnalyzeMethodNode, SyntaxKind.ClassDeclaration);
+        }
+
+        private void AnalyzeMethodNode(SyntaxNodeAnalysisContext context)
+        {
+            analyzer.SetAnalyzerStrategy(new DecoratorAnalyzer.HasSomeInterface(context));
+
+            if (!analyzer.Analyze())
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+        }
+
+    }
+}
