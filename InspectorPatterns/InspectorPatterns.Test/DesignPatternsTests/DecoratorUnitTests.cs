f7c601a05e374457091469acc3611d29d13f28d9
Mohamed Kaddouri <S1172394@student.windesheim.nl>
2023-01-14 20:30:58 +0100

    added final unit test

--- InspectorPatterns/InspectorPatterns.Test/DesignPatternsTests/DecoratorUnitTests.cs
+++ InspectorPatterns/InspectorPatterns.Test/DesignPatternsTests/DecoratorUnitTests.cs
@@ -358,5 +358,62 @@ namespace InspectorPatterns.Test.DesignPatternsTests
             Assert.True((DecoratorAnalyzer._identifierValue == "Beverage"));
             Assert.True(hasAbatractClass && staticField && decoratorAnalyzer);
         }
+
+        [Fact]
+        public void Test_IsDecorator_Should_FAIL()
+        {
+            //Arrange
+            var correctCode = @"
+                    public class Milk: Beverage
+                    {
+                        private string baseBeverage;
+
+                        public Milk(string beverage)
+                        {
+                            this.baseBeverage = beverage;
+                        }
+
+                        public override String Description
+                        {
+                            get
+                            {
+                                return baseBeverage.Description + "", Melk"";
+                            }
+                        }
+
+                        public override double Cost()
+                        {
+                            return 0.35 + baseBeverage.Cost();
+                        }
+                    }
+
+                    abstract class Beverage {
+                        
+                    }";
+
+            var contextNode = SyntaxFactory.ParseSyntaxTree(correctCode).GetRoot();
+
+            var workspace = new AdhocWorkspace()
+                .AddSolution(SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Create(), null, null))
+                .AddProject("DummyProject", "DummyProject", LanguageNames.CSharp)
+                .AddMetadataReference(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                .AddDocument("Decorator.cs", correctCode).Project.Solution;
+
+
+            var dummyProject = workspace.Projects.FirstOrDefault();
+            var cToken = new CancellationToken();
+            var comp = dummyProject.GetCompilationAsync().Result;
+            var sModel = comp.GetSemanticModel(comp.SyntaxTrees.First());
+            var abstractClass = new SyntaxNodeAnalysisContext(contextNode.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault(), sModel, null, null, null, cToken);
+
+            // Act
+            var hasAbatractClass = (new DecoratorAnalyzer.HasAbstractClass(abstractClass)).Analyze();
+            var staticField = (new DecoratorAnalyzer.HasPrivateStaticField(contextNode)).Analyze();
+            var decoratorAnalyzer = (new DecoratorAnalyzer.HasConstructor(contextNode)).Analyze();
+
+            //Assert
+            Assert.True((DecoratorAnalyzer._identifierValue == "Beverage"));
+            Assert.False(hasAbatractClass && staticField && decoratorAnalyzer);
+        }
     }
 }
